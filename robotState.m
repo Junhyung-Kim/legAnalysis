2dab05dbb0c2afe336fe5b8016209dfd658c1ea1
jhk <jhk@jhkui-MacBookAir.local>
2022-02-24 16:14:38 +0900

    robot kinematics

--- robotState.m
+++ robotState.m
@@ -1,1 +1,16 @@
 %load humanoid model
+robot = importrobot("dyros_tocabi_sim.urdf");
+config = homeConfiguration(robot);
+q_init = [0.0; 0.0; -0.55; 1.26; -0.71; 0.0; 0.0; 0.0; -0.55; 1.26; -0.71; 0.0; 0.0; 0.0; 0.0; 0.2; 0.6; 1.5; -1.47; -1.0; 0.0; -1.0; 0.0; 0.0; 0.0; -0.2; -0.6; -1.5; 1.47; 1.0; 0.0; 1.0; 0.0];
+jointnum = size(q_init,1);
+for i = 1:jointnum
+    config(i).JointPosition = q_init(i,1);
+end
+
+PELV = getTransform(robot,config,'Pelvis_Link','Pelvis_Link');
+RF = getTransform(robot,config,'R_Foot_Link','Pelvis_Link');
+LF = getTransform(robot,config,'L_Foot_Link','Pelvis_Link');
+
+
+
+
