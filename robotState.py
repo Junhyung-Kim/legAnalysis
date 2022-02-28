f1a03a218578a3b895b75bd3224e993d651958e2
jhk <john3.16@snu.ac.kr>
2022-02-28 09:38:08 +0900

    contact jacobian

--- robotState.py
+++ robotState.py
@@ -10,12 +10,32 @@ from os.path import dirname, join, abspath
 def talker():
     np.set_printoptions(threshold=sys.maxsize)
     model = pinocchio.buildModelFromUrdf("/home/jhk/legAnalysis/dyros_tocabi_sim.urdf",pinocchio.JointModelFreeFlyer())      
+   
+    LFframe_id = model.getFrameId("L_Foot_Link")
+    RFframe_id = model.getFrameId("R_Foot_Link")
+
+    contactPointLF = pinocchio.SE3.Identity()
+    contactPointRF = pinocchio.SE3.Identity()
+    
+    contactPointLF.translation.T.flat = [0.03, 0, -0.1585]
+    contactPointRF.translation.T.flat = [0.03, 0, -0.1585]
+
+    joint_id = model.getJointId("L_AnkleRoll_Joint")
+    model.addBodyFrame("LF_contact", joint_id, contactPointLF, LFframe_id)
+
+    joint_id = model.getJointId("R_AnkleRoll_Joint")
+    model.addBodyFrame("RF_contact", joint_id, contactPointRF, RFframe_id)
+ 
+    LFcframe_id = model.getFrameId("LF_contact")
+    RFcframe_id = model.getFrameId("RF_contact")
+
     data = model.createData()
     q = pinocchio.randomConfiguration(model)
     q_init = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, -0.55, 1.26, -0.71, 0.0, 0.0, 0.0, -0.55, 1.26, -0.71, 0.0, 0.0, 0.0, 0.0, 0.2, 0.6, 1.5, -1.47, -1.0, 0.0, -1.0, 0.0, 0.0, 0.0, -0.2, -0.6, -1.5, 1.47, 1.0, 0.0, 1.0, 0.0]
 
     for i in range(0, len(q)):
         q[i] = q_init[i]
+
     qdot = pinocchio.utils.zero(model.nv)
     qddot = pinocchio.utils.zero(model.nv)
     qdot_z = pinocchio.utils.zero(model.nv)
@@ -40,28 +60,48 @@ def talker():
     pinocchio.computeCoriolisMatrix(model, data, q, qdot)
     pinocchio.rnea(model, data, q, qdot_z, qddot_z)
 
-    frame_id = model.getFrameId("L_Foot_Link")
-    LF_j = pinocchio.computeFrameJacobian(model,data,q,frame_id,pinocchio.LOCAL_WORLD_ALIGNED)
-    
-    frame_id = model.getFrameId("R_Foot_Link")
-    RF_j = pinocchio.computeFrameJacobian(model,data,q,frame_id,pinocchio.LOCAL_WORLD_ALIGNED)
 
     contactState = 1
 
-    #contactPoint[0] = [0.03, 0.0, -0.0135]
-    #contactPoint[1] = [0.03, 0.0, -0.0135]
-    #contactPoint[2] = [0.03, 0.0, -0.035]
-    #contactPoint[3] = [0.03, 0.0, -0.035]
-    
+    if contactState == 1:
+        print("double support")
+    elif contactState == 2:
+        print("RF support")
+    else:
+        print("LF support")    
 
     #COR_slice = data.C[:6:39]
     #M_slice = data.M[:6:39]
     #g_slice = data.tau[6:39]
-    
+
 
     M = data.M
     COR = data.C
     G = data.tau
+
+    LF_j = pinocchio.computeFrameJacobian(model,data,q,LFframe_id,pinocchio.LOCAL_WORLD_ALIGNED)    
+    RF_j = pinocchio.computeFrameJacobian(model,data,q,RFframe_id,pinocchio.LOCAL_WORLD_ALIGNED)
+
+    aa = model.getFrameId("LF_contact")
+    print(aa)
+
+    LF_cj = pinocchio.computeFrameJacobian(model,data,q,LFcframe_id,pinocchio.LOCAL_WORLD_ALIGNED)    
+    RF_cj = pinocchio.computeFrameJacobian(model,data,q,RFcframe_id,pinocchio.LOCAL_WORLD_ALIGNED)
+
+    print(LF_cj)
+
+    #  joint_id = model.getFrameId("L_Foot_Joint")
+  #  model.addJoint(joint_id, pinocchio.JointModel, pinocchio.SE3.Identity(),"L_Foot_Joint1")
+  
+  #  print("\nJoint placements:")
+   # for name, oMi in zip(model.names, data.oMi):
+    #    print(("{:<24} : {: .2f} {: .2f} {: .2f}"
+     #       .format( name, *oMi.translation.T.flat )))
+
+   # print("\nframe placements:")
+   # for name, oMf in zip(model.frames, data.oMf):
+    #    print(("{:<24} : {: .2f} {: .2f} {: .2f}"
+     #       .format( name, *oMf.translation.T.flat )))
 
     #for i in range(0,6):
      #   for j in range(0,39):
