3cb364f5b3bda50a927cf1fc39a7b3e597a1c50f
jhk <john3.16@snu.ac.kr>
2022-03-10 13:19:31 +0900

    footstep python & dynamics ok

--- robotState.py
+++ robotState.py
@@ -6,6 +6,57 @@ import numpy as np
 from sys import argv
 from os.path import dirname, join, abspath
 
+def walkingSetup():
+    global x_direction, step_length, hz, total_tick, foot_step_number, final_step_length, t_total_t, t_temp_t, t_double, t_start, t_total, t_temp, t_last, t_double_1, t_double_2
+    global zc, wn, current_step_num, ref_zmp, walking_tick, total_tick
+    hz = 1000
+    x_direction = 1.0
+    step_length = 0.1
+    foot_step_number = int(round(x_direction / step_length))
+    final_step_length = x_direction % step_length
+
+    if x_direction % step_length != 0:
+        foot_step_number = foot_step_number + 1   
+    
+    t_total_t = 1.2
+    t_temp_t = 1.0
+    t_double = 0.1
+    total_tick = 80000
+
+    t_total = t_total_t * hz
+    t_temp = t_temp_t * hz
+    t_start = t_temp + 1
+    t_last = t_temp + t_total
+    t_double_1 = 0.1 * hz
+    t_double_2 = 0.1 * hz
+
+    current_step_num = 0
+    zc = 0.727822
+    wn = np.sqrt(9.81/zc)
+
+    ref_zmp = np.zeros((total_tick,2));
+    walking_tick = np.zeros((1,total_tick));
+
+def footStep(): 
+    global foot_step
+    foot_step = np.zeros((foot_step_number + 1, 2))
+
+    foot_step[0,0] = LF_tran[0]
+    foot_step[0,1] = LF_tran[1]
+
+    for i in range(1,foot_step_number + 1):
+        foot_step[i,0] = step_length + foot_step[i-1,0]
+        foot_step[i,1] = foot_step[i-1,1] * (-1)
+
+    if final_step_length == 0:
+        foot_step[foot_step_number,0] = step_length + foot_step[foot_step_number - 1,0]
+        foot_step[foot_step_number,1] = foot_step[foot_step_number - 1,1] * (-1)
+    else:
+        foot_step[foot_step_number,0] = final_step_length + foot_step[foot_step_number - 1,0]
+        foot_step[foot_step_number,1] = foot_step[foot_step_number - 1,1] * (-1)
+
+#def zmpGenerator():
+
 
 def talker():
     np.set_printoptions(threshold=sys.maxsize)
@@ -31,7 +82,7 @@ def talker():
 
     data = model.createData()
     q = pinocchio.randomConfiguration(model)
-    q_init = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, -0.55, 1.26, -0.71, 0.0, 0.0, 0.0, -0.55, 1.26, -0.71, 0.0, 0.0, 0.0, 0.0, 0.2, 0.6, 1.5, -1.47, -1.0, 0.0, -1.0, 0.0, 0.0, 0.0, -0.2, -0.6, -1.5, 1.47, 1.0, 0.0, 1.0, 0.0]
+    q_init = [0.0, 0.0, 0.814175, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, -0.56, 1.3, -0.73, 0.0, 0.0, 0.0, -0.56, 1.3, -0.73, 0.0, 0.0, 0.0, 0.0, 0.2, 0.6, 1.5, -1.47, -1.0, 0.0, -1.0, 0.0, 0.0, 0.0, -0.2, -0.6, -1.5, 1.47, 1.0, 0.0, 1.0, 0.0]
 
     for i in range(0, len(q)):
         q[i] = q_init[i]
@@ -46,6 +97,9 @@ def talker():
     pinocchio.updateFramePlacements(model,data)
     pinocchio.computeJointJacobians(model, data, q)
     pinocchio.computeMinverse(model, data, q)
+    
+    global LF_tran
+    global RF_tran
 
     LF_tran = data.oMi[7].translation
     RF_tran = data.oMi[13].translation
@@ -108,45 +162,28 @@ def talker():
     robotJcinvT = np.matmul(np.matmul(robotLambdac, robotJac),Minv)
     robotNc = np.subtract(np.identity(model.nv),np.matmul(np.transpose(robotJac),robotJcinvT))
     robotPc = np.matmul(robotJcinvT,G)
-    robotW = np.matmul(Minv[6:6+modeldof,0:model.nq],robotNc[0:model.nq,6:6+modeldof])
-    robotWinv = np.linalg.inv(robotW)
+   # robotW = np.matmul(Minv[6:6+modeldof,0:model.nq],robotNc[0:model.nq,6:6+modeldof])
+   # robotWinv = np.linalg.inv(robotW)
+
     robotmuc = np.matmul(robotLambdac,np.subtract(np.matmul(np.matmul(robotJac,Minv),b),np.matmul(robotdJac,qdot)))
     robothc = np.matmul(np.transpose(robotJac),np.add(robotmuc, robotPc))
 
     robotContactForce = pinocchio.utils.zero(12)
 
-    robotTorque = np.matmul(np.linalg.inv(robotNc),np.subtract(np.add(np.add(np.matmul(M,qddot),b),G),robothc))
-    #print(robotTorque)
+    robotTorque = np.matmul(np.linalg.pinv(robotNc),np.subtract(np.add(np.add(np.matmul(M,qddot),b),G),robothc))
 
     if contactState == 1:
-        print("ss")
         robotContactForce = np.subtract(np.subtract(np.matmul(robotJcinvT,robotTorque), robotPc),robotmuc)
     elif contactState == 2:
-        print("RF support")
         robotContactForce[6:12] = np.subtract(np.subtract(np.matmul(robotJcinvT,robotTorque), robotPc),robotmuc)
-    else:
-        print("LF support")    
+    else:   
         robotContactForce[0:6] = np.subtract(np.subtract(np.matmul(robotJcinvT,robotTorque), robotPc),robotmuc)
 
+    walkingSetup()
+    footStep()
+
+    print(robotTorque)
     print(robotContactForce)
-    #  joint_id = model.getFrameId("L_Foot_Joint")
-  #  model.addJoint(joint_id, pinocchio.JointModel, pinocchio.SE3.Identity(),"L_Foot_Joint1")
-  
-  #  print("\nJoint placements:")
-   # for name, oMi in zip(model.names, data.oMi):
-    #    print(("{:<24} : {: .2f} {: .2f} {: .2f}"
-     #       .format( name, *oMi.translation.T.flat )))
-
-   # print("\nframe placements:")
-   # for name, oMf in zip(model.frames, data.oMf):
-    #    print(("{:<24} : {: .2f} {: .2f} {: .2f}"
-     #       .format( name, *oMf.translation.T.flat )))
-
-    #for i in range(0,6):
-     #   for j in range(0,39):
-      #      print(data.J[i][j], end=' ')
-       # print("\n")    
-
   
 if __name__=='__main__':
     talker()
